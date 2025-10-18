1a8b065e18f4a3b5302c91d1f5b89e5dfceea9c8
Yasin Bilgin <stdxpe@gmail.com>
2025-10-18 19:22:37 +0300

    test test 19.22

--- feedmate/lib/main.dart
+++ feedmate/lib/main.dart
@@ -13,8 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Feed Mate',
-      debugShowCheckedModeBanner: false,
       routerConfig: AppRouter.router,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
